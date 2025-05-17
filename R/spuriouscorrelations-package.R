98d4355450f3635a340402a3aa49ef3434849721
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2025-05-17 16:57:30 -0400

    sent to CRAN

--- R/spuriouscorrelations-package.R
+++ R/spuriouscorrelations-package.R
@@ -9,6 +9,8 @@
 #'  \item{var2}{Variable 2}
 #'  \item{var1_short}{Variable 1 short name}
 #'  \item{var2_short}{Variable 2 short name}
+#'  \item{var1_unit}{Variable 1 unit}
+#'  \item{var2_unit}{Variable 2 unit}
 #'  \item{var1_value}{Variable 1 value}
 #'  \item{var2_value}{Variable 2 value}
 #'  \item{source}{Source}
